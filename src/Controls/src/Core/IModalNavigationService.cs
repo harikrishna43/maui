56361af975b4d7544063b1a83dd6e650a7917c68
Shane Neuville <shneuvil@microsoft.com>
2021-06-17 16:58:29 -0500

    iOS Modal Changes

---
+++ src/Controls/src/Core/IModalNavigationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.Controls
+{
+	// But why did you make it internal? 
+	// The implementation of these services inside Xamarin.Forms is also internal and
+	// we aren't ready to commit to a public navigation set of interfaces yet
+	// Hopefully this comment won't exist anymore for v1 .NET MAUI 
+	internal interface IModalNavigationService
+	{
+		IReadOnlyList<Page> ModalStack { get; }
+		Task PushModalAsync(Page page);
+		Task PushModalAsync(Page page, bool animated);
+		Task<Page> PopModalAsync();
+		Task<Page> PopModalAsync(bool animated);
+	}
+}
---
+++ src/Controls/src/Core/Platform/iOS/ModalNavigationService.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using Microsoft.Maui.Controls.Internals;
+using UIKit;
+
+namespace Microsoft.Maui.Controls.Platform
+{
+	class ModalNavigationService : IModalNavigationService
+	{
+		// this is wrong and temporary
+		Page Page => Application.Current.Windows[0].View as Page;
+
+		readonly List<Page> _modals;
+		readonly IMauiContext _mauiContext;
+		bool _appeared = true;
+		public ModalNavigationService(IMauiContext mauiContext)
+		{
+			_modals = new List<Page>();
+			_mauiContext = mauiContext;
+		}
+
+		// do I really need this anymore?
+		static void HandleChildRemoved(object sender, ElementEventArgs e)
+		{
+			var view = e.Element;
+			//view?.DisposeModalAndChildRenderers();
+		}
+
+
+		public IReadOnlyList<Page> ModalStack => _modals;
+
+		public Task<Page> PopModalAsync()
+		{
+			return PopModalAsync(true);
+		}
+
+		public async Task<Page> PopModalAsync(bool animated)
+		{
+			var modal = _modals.Last();
+			_modals.Remove(modal);
+			modal.DescendantRemoved -= HandleChildRemoved;
+
+			var controller = (modal.Handler as INativeViewHandler).ViewController;
+
+			//if (_modals.Count >= 1 && controller != null)
+				await controller.DismissViewControllerAsync(animated);
+			/*
+			 * I don't know what this does
+			 * else
+				await _renderer.DismissViewControllerAsync(animated);*/
+
+			// Yes?
+			//modal.DisposeModalAndChildRenderers();
+
+			if (!IsModalPresentedFullScreen(modal))
+				Page.GetCurrentPage()?.SendAppearing();
+
+			return modal;
+		}
+
+		public Task PushModalAsync(Page modal)
+		{
+			return PushModalAsync(modal, true);
+		}
+
+		public Task PushModalAsync(Page modal, bool animated)
+		{
+			EndEditing();
+
+			var elementConfiguration = modal as IElementConfiguration<Page>;
+
+			var presentationStyle = elementConfiguration?.On<PlatformConfiguration.iOS>()?.ModalPresentationStyle().ToNativeModalPresentationStyle();
+
+			bool shouldFire = true;
+
+			if (NativeVersion.IsAtLeast(13))
+			{
+				if (presentationStyle == UIKit.UIModalPresentationStyle.FullScreen)
+					shouldFire = false; // This is mainly for backwards compatibility
+			}
+			else
+			{
+				// While the above IsiOS13OrNewer will always be false if __XCODE11__ is true
+				// the UIModalPresentationStyle.Automatic is the only Xcode 11 API
+				// for readability I decided to only take this part out
+				if (presentationStyle == UIKit.UIModalPresentationStyle.Automatic)
+					shouldFire = false;
+
+				if (presentationStyle == UIKit.UIModalPresentationStyle.FullScreen)
+					shouldFire = false; // This is mainly for backwards compatibility
+			}
+
+			if (_appeared && shouldFire)
+				Page.GetCurrentPage()?.SendDisappearing();
+
+			_modals.Add(modal);
+
+			modal.DescendantRemoved += HandleChildRemoved;
+
+			if (_appeared)
+				return PresentModal(modal, animated && animated);
+
+			return Task.FromResult<object>(null);
+		}
+
+		static bool IsModalPresentedFullScreen(Page modal)
+		{
+			var elementConfiguration = modal as IElementConfiguration<Page>;
+			var presentationStyle = elementConfiguration?.On<PlatformConfiguration.iOS>()?.ModalPresentationStyle();
+			return presentationStyle != null && presentationStyle == PlatformConfiguration.iOSSpecific.UIModalPresentationStyle.FullScreen;
+		}
+
+		async Task PresentModal(Page modal, bool animated)
+		{
+			var modalRenderer = modal.ToNative(_mauiContext);
+
+			var wrapper = new ModalWrapper(modal.Handler as INativeViewHandler);
+
+			if (_modals.Count > 1)
+			{
+				var topPage = _modals[_modals.Count - 2];
+				var controller = (topPage.Handler as INativeViewHandler).ViewController;
+				if (controller != null)
+				{
+					await controller.PresentViewControllerAsync(wrapper, animated);
+					await Task.Delay(5);
+					return;
+				}
+			}
+
+			// One might wonder why these delays are here... well thats a great question. It turns out iOS will claim the 
+			// presentation is complete before it really is. It does not however inform you when it is really done (and thus 
+			// would be safe to dismiss the VC). Fortunately this is almost never an issue
+
+			var _renderer = UIApplication.SharedApplication.GetKeyWindow().RootViewController;
+			await _renderer.PresentViewControllerAsync(wrapper, animated);
+			await Task.Delay(5);
+		}		
+
+		void EndEditing()
+		{
+			// If any text entry controls have focus, we need to end their editing session
+			// so that they are not the first responder; if we don't some things (like the activity indicator
+			// on pull-to-refresh) will not work correctly. 
+
+			// The topmost modal on the stack will have the Window; we can use that to end any current
+			// editing that's going on 
+			if (_modals.Count > 0)
+			{
+				var uiViewController = (_modals.Last().Handler as INativeViewHandler).ViewController;
+				uiViewController?.View?.Window?.EndEditing(true);
+				return;
+			}
+
+
+			// TODO MAUI
+			// If there aren't any modals, then the platform renderer will have the Window
+			//_renderer.View?.Window?.EndEditing(true);
+		}
+	}
+}
